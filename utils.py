522b25e54e187e71f17b8af75caca52086dad60d
dmphuongdoan <duymyphuong.doan@studenti.unimi.it>
2025-11-24 12:39:08 +0100

    changes

--- utils.py
+++ utils.py
@@ -8,6 +8,10 @@ import ast
 credits = pd.read_csv('/Users/phuongdoan/Downloads/dataset/credits.csv')
 #print(df_credits['cast'].head(10))
 #print(type(df_credits['cast'][0]))
+keywords = pd.read_csv('/Users/phuongdoan/Downloads/dataset/keywords.csv')
+movies = pd.read_csv('/Users/phuongdoan/Downloads/dataset/movies_metadata.csv',low_memory = False)
+ratings = pd.read_csv('/Users/phuongdoan/Downloads/dataset/ratings.csv')
+
 
 def parse_json_column(credits, column_name):
     """
@@ -28,7 +32,33 @@ def parse_json_column(credits, column_name):
 credits['cast_names'] = parse_json_column(credits, 'cast')
 credits['crew_names'] = parse_json_column(credits, 'crew')
 
-print(credits[['cast_names','crew_names']].head())
+#print(credits[['cast_names','crew_names', 'id']].head())
 
+def parse_json_column(keywords, column_name):
+    """
+    Parse a stringified JSON column into a list of 'name' values.
+    """
+    def parse_cell(x):
+        if pd.notnull(x):
+            try:
+                data = ast.literal_eval(x)  # Đổi từ json.loads sang ast.literal_eval
+                return [item['name'] for item in data]
+            except (ValueError, TypeError, KeyError):
+                return []
+        else:
+            return []
+    
+    return keywords[column_name].apply(parse_cell)
+keywords['keywords'] = parse_json_column(keywords,'keywords')
+#print(keywords[['id','keywords']].head())
 
+def format_currency(value):
+    """Format budget value and revenue value to currency"""
+    if pd.notnull(value) and value > 0:
+        return f"${value:,.0f}"
+    return "N/A"
 
+movies['budget'] = movies['budget'].apply(format_currency)
+movies['revenue'] = movies['revenue'].apply(format_currency)
+
+print(movies[['budget','revenue']].head())
